c1e9499ab8cbe8666c3729324c0d8a633d477446
Francesca Bruno (KiWiiU) <piranaplant4smash@gmail.com>
2024-09-21 19:11:25 -0400

    wrapping up

--- Assets/Scripts/W3Manager.cs
+++ Assets/Scripts/W3Manager.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class W3Manager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject D2, D3, D4, D5, E;
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+
+            if (Holder.endHalf[0])
+                D2.SetActive(true);
+
+            if (Holder.endHalf[1])
+                D3.SetActive(true);
+
+            if (Holder.endHalf[2])
+                D4.SetActive(true);
+
+            if (Holder.endHalf[3])
+                D5.SetActive(true);
+
+            if (Holder.endHalf[4])
+                E.SetActive(true);
+        }
     }
 }
