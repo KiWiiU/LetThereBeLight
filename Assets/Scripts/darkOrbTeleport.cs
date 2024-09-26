6e19eb2b3d35e5d5efdffffd2a97973e207b2ed7
Francesca Bruno (KiWiiU) <135619100+KiWiiU@users.noreply.github.com>
2024-09-26 16:12:19 -0400

    secretsStart

--- Assets/Scripts/darkOrbTeleport.cs
+++ Assets/Scripts/darkOrbTeleport.cs
@@ -13,45 +13,6 @@ public class darkOrbTeleport : MonoBehaviour
     void OnTriggerEnter(Collider other){
 // when the object touches something, it will check if it is the player. If it is, it will "teleport" by loading a scene.
        if(other.CompareTag("Player")){
-         switch(change){
-// this switch statment is used to tell the holder variable that a level has been cleared. Since this is a function, "change" is the parameter that represents the number level that was cleared.
-           case 1:
-             Holder.L[0] = true;
-             break;
-           case 2:
-             Holder.L[1] = true;
-             break;
-           case 3:
-             Holder.L[2] = true;
-             break;
-           case 4:
-             Holder.L[3] = true;
-             break;
-           case 5:
-             Holder.L[4] = true;
-             break;
-           case 6:
-             Holder.W2C1 = true;
-             break;
-           case 7: 
-             Holder.W2C2 = true;
-             break;
-           case 8:
-             Holder.W2C3 = true;
-             break;
-           case 9:
-             Holder.W2C4 = true;
-             break;
-           case 10:
-             Holder.W2C5 = true;
-             break;
-           case 11:
-             Holder.endHalf[0] = true;
-             break;
-           default:
-             break;
-         }
-
 	 switch (w){
             case 0:
               altGo = Holder.L[0];
